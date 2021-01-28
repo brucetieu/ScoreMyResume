2b7f7f8b3534ccf5a55399a48d8a94bca9482eab
brucetieu <brucetieu5@gmail.com>
2021-01-28 15:28:32 -0700

    Initial commit

---
+++ src/main/java/com/bruce/jobmatchr/JobmatchrApplication.java
@@ -0,0 +1,13 @@
+package com.bruce.jobmatchr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobmatchrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobmatchrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bruce/jobmatchr/JobmatchrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bruce.jobmatchr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobmatchrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
