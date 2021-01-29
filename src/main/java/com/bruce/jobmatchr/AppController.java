8701503ccf850dc483ace45fc7f482f891a273b1
brucetieu <brucetieu5@gmail.com>
2021-01-29 11:13:45 -0700

    Add controller

---
+++ src/main/java/com/bruce/jobmatchr/AppController.java
@@ -0,0 +1,16 @@
+package com.bruce.jobmatchr;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AppController {
+
+    @GetMapping("/")
+    public String viewHomepage() {
+        return "index";
+    }
+
+    
+}
