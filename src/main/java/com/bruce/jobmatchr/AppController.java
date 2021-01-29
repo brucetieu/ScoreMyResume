d117055485a38b22908112c1ca06bf09135a7d78
brucetieu <brucetieu5@gmail.com>
2021-01-29 11:36:58 -0700

    Add registration success handler and html

--- src/main/java/com/bruce/jobmatchr/AppController.java
+++ src/main/java/com/bruce/jobmatchr/AppController.java
@@ -1,12 +1,17 @@
 package com.bruce.jobmatchr;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AppController {
+
+    @Autowired
+    private UserRepository userRepository;
 
     @GetMapping("/")
     public String viewHomepage() {
@@ -17,6 +22,14 @@ public class AppController {
     public String showSignUpForm(Model model) {
         model.addAttribute("user", new User());
         return "signup_form";
+    }
+
+    @PostMapping("/process_register")
+    public String processRegistration(User user) {
+        // Save the registered user in the db
+        userRepository.save(user);
+
+        return "registration_success";
     }
 
 }
