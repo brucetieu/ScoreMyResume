53c3a448b49d4b5a13b433400412a71f03323df9
brucetieu <brucetieu5@gmail.com>
2021-01-29 11:50:41 -0700

    Add CustomUserDetailsService, which authenticates a logged user

--- src/main/java/com/bruce/jobmatchr/CustomUserDetailsService.java
+++ src/main/java/com/bruce/jobmatchr/CustomUserDetailsService.java
@@ -1,2 +1,24 @@
-package com.bruce.jobmatchr;public class CustomUserDetailsService {
+package com.bruce.jobmatchr;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+// Implement the loaduser method. Perform authentication for logging in
+public class CustomUserDetailsService implements UserDetailsService {
+
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user= userRepository.findByEmail(email);
+        if (user == null) throw new UsernameNotFoundException("User not found");
+
+        // Otherwise, we authenticate this user
+        return new CustomUserDetails(user);
+
+    }
 }
