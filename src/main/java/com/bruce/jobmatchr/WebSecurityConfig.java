a4fbcbc86db6d76ff5112e36a7ac36716d0bd914
brucetieu <brucetieu5@gmail.com>
2021-01-29 13:46:37 -0700

    Add spring security

--- src/main/java/com/bruce/jobmatchr/WebSecurityConfig.java
+++ src/main/java/com/bruce/jobmatchr/WebSecurityConfig.java
@@ -1,2 +1,62 @@
-package com.bruce.jobmatchr;public class WebSecurityConfig {
+package com.bruce.jobmatchr;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource; // connect to database for authentication
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new CustomUserDetailsService();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return authProvider;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(authenticationProvider());
+    }
+
+    // Configure login and logout
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/list_users").authenticated()  // only logged in users can see this
+                .anyRequest().permitAll()  // Only this page is secure
+                .and()
+                .formLogin()
+                .usernameParameter("email")
+                .defaultSuccessUrl("/list_users") // direct after successful auth
+                .permitAll() // Allow user to see the page
+                .and()
+                .logout().logoutSuccessUrl("/").permitAll(); // DIrect user to / after logged out
+    }
 }
+
