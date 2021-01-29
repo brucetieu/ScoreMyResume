8c58525e5cc38ec947d8ee257bf54fe4878ae33e
brucetieu <brucetieu5@gmail.com>
2021-01-29 11:29:44 -0700

    Add handler to signup_form

--- src/main/java/com/bruce/jobmatchr/AppController.java
+++ src/main/java/com/bruce/jobmatchr/AppController.java
@@ -2,6 +2,7 @@ package com.bruce.jobmatchr;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -12,5 +13,10 @@ public class AppController {
         return "index";
     }
 
-    
+    @GetMapping("/register")
+    public String showSignUpForm(Model model) {
+        model.addAttribute("user", new User());
+        return "signup_form";
+    }
+
 }
