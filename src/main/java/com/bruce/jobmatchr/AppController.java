073ae0d7636e9d5836cf626ecb693759d62a857b
brucetieu <brucetieu5@gmail.com>
2021-01-29 14:46:01 -0700

    Add bcrypt

--- src/main/java/com/bruce/jobmatchr/AppController.java
+++ src/main/java/com/bruce/jobmatchr/AppController.java
@@ -2,10 +2,13 @@ package com.bruce.jobmatchr;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class AppController {
@@ -27,9 +30,20 @@ public class AppController {
     @PostMapping("/process_register")
     public String processRegistration(User user) {
         // Save the registered user in the db
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String encodedPassword = encoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+
         userRepository.save(user);
 
         return "registration_success";
     }
 
+    @GetMapping("/list_users")
+    public String viewUsersList(Model model) {
+        List<User> listUsers = userRepository.findAll();
+        model.addAttribute("listUsers", listUsers);
+
+        return "users";
+    }
 }
--- src/main/java/com/bruce/jobmatchr/PasswordEncoder.java
+++ src/main/java/com/bruce/jobmatchr/PasswordEncoder.java
@@ -1,2 +1,13 @@
-package com.bruce.jobmatchr;public class PasswordEncoder {
+package com.bruce.jobmatchr;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordEncoder {
+
+    public static void main(String[] args) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String rawPassword = "test";
+        String encodedPassword = encoder.encode(rawPassword);
+        System.out.println(encodedPassword);
+    }
 }
