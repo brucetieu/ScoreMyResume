05e95b77b2241ed2feb0024bd5785f35323f585b
brucetieu <brucetieu5@gmail.com>
2021-01-29 13:46:25 -0700

    Add comment

--- src/main/java/com/bruce/jobmatchr/UserRepository.java
+++ src/main/java/com/bruce/jobmatchr/UserRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.Query;
 
 //
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    // Run this query every time we find user by email
     @Query("SELECT u FROM User u WHERE u.email = ?1")
     User findByEmail(String email);
 }
