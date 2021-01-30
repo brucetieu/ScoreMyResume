ef008893bc53a1c160ddfcbe3c179a271138f8da
brucetieu <brucetieu5@gmail.com>
2021-01-30 13:59:14 -0700

    Add methods for updating and getting the password

--- src/main/java/com/bruce/jobmatchr/CustomUserDetailsService.java
+++ src/main/java/com/bruce/jobmatchr/CustomUserDetailsService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 // Implement the loaduser method. Perform authentication for logging in
 public class CustomUserDetailsService implements UserDetailsService {
@@ -20,5 +21,37 @@ public class CustomUserDetailsService implements UserDetailsService {
         // Otherwise, we authenticate this user
         return new CustomUserDetails(user);
 
+    }
+
+    // Update password via setting a reset password token by email
+    public void updateResetPassword(String token, String email) throws UserNotFoundException {
+        User user = userRepository.findByEmail(email);
+
+        if (user != null) {
+            user.setResetPasswordToken(token);
+            userRepository.save(user);
+        } else {
+            throw new UserNotFoundException("Could not find any User with email " + email);
+        }
+
+    }
+
+    // Get the customer by the random password token
+    public User get(String resetPasswordToken) {
+
+        // Used by controller layer to check if a customer belongs to the given password token or not.
+        return userRepository.findByResetPasswordToken(resetPasswordToken);
+    }
+
+    // Update the password of a user
+    public void updatePassword(User user, String newPassword) {
+        // Encrypt password
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String encodedPassword = passwordEncoder.encode(newPassword);
+
+        user.setPassword(encodedPassword);
+        user.setResetPasswordToken(null);
+
+        userRepository.save(user);
     }
 }
