3d0a1755be181159f599cc2ba847dd0b6f8d1f53
brucetieu <brucetieu5@gmail.com>
2021-01-30 16:40:20 -0700

    Add logic to send the email

--- src/main/java/com/bruce/jobmatchr/ForgotPasswordController.java
+++ src/main/java/com/bruce/jobmatchr/ForgotPasswordController.java
@@ -2,19 +2,27 @@ package com.bruce.jobmatchr;
 
 import net.bytebuddy.utility.RandomString;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
+import java.io.UnsupportedEncodingException;
 
 @Controller
 public class ForgotPasswordController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private JavaMailSender mailSender;
 
     @RequestMapping("/forgot_password")
     public String showForgotPasswordForm(Model model) {
@@ -40,15 +48,45 @@ public class ForgotPasswordController {
             System.out.println(resetPasswordLink);
 
             // send email
+            sendEmail(email, resetPasswordLink);
+
+            // Pass in a success message to the form
+            model.addAttribute("message", "We have sent  reset password link to your email. Please check.");
+
 
         } catch (UserNotFoundException e) {
             model.addAttribute("error", e.getMessage());
+        } catch (UnsupportedEncodingException | MessagingException e) {
+            model.addAttribute("error", "Error while sending email.");
         }
 
         System.out.println("Email: " + email);
         System.out.println("Token: " + token);
 
         return "forgot_password_form";
+    }
+
+    // Send the password reset email.
+    private void sendEmail(String email, String resetPasswordLink) throws UnsupportedEncodingException, MessagingException {
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message);
+
+        helper.setFrom("contact@jobmatchr.com", "JobMatchr Support");
+        helper.setTo(email);
+
+        String subject = "Here's the link to reset your password";
+
+        String content = "<p> Hello,</p> "
+                + "<p>You have requested to reset your password.</p>"
+                + "<p>Click the link below to change your password.</p>"
+                + "<p><b><a href=\"" + resetPasswordLink + "\">Change my password</a><b></p>"
+                + "<p>Ignore this email if you remembered your password, or you have not made this request.</p>";
+
+        helper.setSubject(subject);
+        helper.setText(content, true);
+
+        mailSender.send(message);
+
     }
 
 }
