843d8b775fde2e94a20a523b749158725c183fb6
brucetieu <brucetieu5@gmail.com>
2021-01-30 13:58:42 -0700

    Add forgotPasswordController

--- src/main/java/com/bruce/jobmatchr/ForgotPasswordController.java
+++ src/main/java/com/bruce/jobmatchr/ForgotPasswordController.java
@@ -1,2 +1,17 @@
-package com.bruce.jobmatchr;public class ForgotPasswordController {
+package com.bruce.jobmatchr;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ForgotPasswordController {
+
+    @RequestMapping("/forgot_password")
+    public String showForgotPasswordForm(Model model) {
+
+        return "forgot_password_form";
+    }
+
 }
+
