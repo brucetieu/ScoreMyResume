835ccbf6997f3f40d8e3e5fcdc3089942ed91dd9
brucetieu <brucetieu5@gmail.com>
2021-02-01 18:40:11 -0700

    Pagination

--- src/main/java/com/bruce/jobmatchr/webscrape/GlassdoorScrape.java
+++ src/main/java/com/bruce/jobmatchr/webscrape/GlassdoorScrape.java
@@ -9,24 +9,77 @@ import java.io.IOException;
 
 public class GlassdoorScrape {
 
-    public final static String URL = "https://www.indeed.com/jobs?q=";
-    public static void scrape(String jobTitle, String jobLocation) {
+    private final String INDEED = "https://www.indeed.com";
+    private final String QUERY_URL = INDEED + "/jobs?q=";
+    private final String jobDiv = "div.jobsearch-SerpJobCard";
 
-        String queryURL = URL + jobTitle + "&l=" + jobLocation;
-        System.out.println(queryURL);
+
+    public void scrape(String jobTitle, String jobLocation) {
+
+        String fullQueryURL = QUERY_URL + jobTitle + "&l=" + jobLocation;
+        System.out.println(fullQueryURL);
 
         try {
-            Document document = Jsoup.connect(queryURL).get();
+            Document document = Jsoup.connect(fullQueryURL).get();
 
-            Elements jobCard = document.select("div.jobsearch-SerpJobCard");
+            Elements pagination = document.select("ul.pagination-list > li > a");
 
-            for (Element e : jobCard) {
-                String href = e.select("a.jobtitle").attr("href");
-                String pageAd = "https://www.indeed.com" + href;
-                Document doc = Jsoup.connect(pageAd).get();
-                System.out.println(doc.select("h1.jobsearch-JobInfoHeader-title").text());
+            for (Element p : pagination) {
+                String url = p.attr("href");
+                System.out.println(url);
+
+                Document page = Jsoup.connect(INDEED + url).get();
+
+                Elements jobCard = page.select(jobDiv);
+
+                for (Element e : jobCard) {
+                    String title = e.select("h2.title").text();
+                    String company = e.select("span.company").text();
+                    String location = e.select("div.location").text();
+
+                    String href = e.select("a.jobtitle").attr("href");
+                    String pageAd = INDEED + href;
+
+                    Document doc = Jsoup.connect(pageAd).get();
+
+                    String qualifications = doc.select("ul.jobsearch-ReqAndQualSection-item--wrapper").text();
+                    String jobDescription = doc.getElementById("jobDescriptionText").text();
+
+                    System.out.println("Link: " + pageAd);
+                    System.out.println("Title: " + title);
+                    System.out.println("Company: " + company);
+                    System.out.println("Location: " + location);
+                    System.out.println("Qualifications: " + qualifications);
+                    System.out.println("Job description: " + jobDescription);
+                    System.out.println();
+
+
+//            Elements jobCard = document.select(jobDiv);
+//
+//            for (Element e : jobCard) {
+//                String title = e.select("h2.title").text();
+//                String company = e.select("span.company").text();
+//                String location = e.select("div.location").text();
+//
+//                String href = e.select("a.jobtitle").attr("href");
+//                String pageAd = INDEED + href;
+//
+//                Document doc = Jsoup.connect(pageAd).get();
+//
+//                String qualifications = doc.select("ul.jobsearch-ReqAndQualSection-item--wrapper").text();
+//                String jobDescription = doc.getElementById("jobDescriptionText").text();
+//
+//                System.out.println("Link: " + href);
+//                System.out.println("Title: " + title);
+//                System.out.println("Company: " + company);
+//                System.out.println("Location: " + location);
+//                System.out.println("Qualifications: " + qualifications);
+//                System.out.println("Job description: " + jobDescription);
+//                System.out.println();
+
+                }
             }
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
