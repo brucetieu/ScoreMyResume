85359c4ee8110175ae0cab9c7e35e5256111c69f
brucetieu <brucetieu5@gmail.com>
2021-02-11 15:25:36 -0700

    Add TextDocument

--- src/main/java/com/bruce/jobmatchr/parse/TextDocument.java
+++ src/main/java/com/bruce/jobmatchr/parse/TextDocument.java
@@ -1,2 +1,20 @@
-package com.bruce.jobmatchr.parse;public class TextDocument {
+package com.bruce.jobmatchr.parse;
+
+import java.io.File;
+
+public class TextDocument {
+
+    private File resumeFile;
+    private String jobText;
+
+    public TextDocument(File resumeFile) {
+        this.resumeFile = resumeFile;
+    }
+
+    public TextDocument(String jobText) {
+        this.jobText = jobText;
+    }
+
+    public TextDocument() {
+    }
 }
