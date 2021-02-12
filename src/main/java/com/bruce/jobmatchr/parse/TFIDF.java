c6ca58b2472b7e6e12458fa7f7ce28330f40407b
brucetieu <brucetieu5@gmail.com>
2021-02-12 16:13:25 -0700

    Add API

--- src/main/java/com/bruce/jobmatchr/parse/TFIDF.java
+++ src/main/java/com/bruce/jobmatchr/parse/TFIDF.java
@@ -1,2 +1,38 @@
-package com.bruce.jobmatchr.parse;public class TFIDF {
+package com.bruce.jobmatchr.parse;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Set;
+
+public class TFIDF {
+    private List<String> wordsFromResume;
+    private List<String> wordsFromJobDescription;
+    private Set<String> unionOfWords;
+
+
+    public TFIDF(File resumeFile, String jobDescriptionText) throws IOException {
+        this.wordsFromResume = TextExtraction.extractPDFText(resumeFile);
+        this.wordsFromJobDescription = TextExtraction.extractText(jobDescriptionText);
+        unionOfWords.addAll(this.wordsFromResume);
+        unionOfWords.addAll(this.wordsFromJobDescription);
+    }
+
+    public Hashtable<String, Double> getFrequencyByWord() {
+
+    }
+
+    public Hashtable<String, Double> computeTF(List<String> listOfWords) {
+
+    }
+
+    public Hashtable<String, Double> computeIDF() {
+
+    }
+
+    public Hashtable<String, Double> computeTFIDF(Hashtable<String, Double> tf) {
+
+    }
 }
