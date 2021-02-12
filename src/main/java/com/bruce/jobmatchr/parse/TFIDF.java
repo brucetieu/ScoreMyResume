d1384f6b289754f3b37bbbd1a0bc320106a8b5b4
brucetieu <brucetieu5@gmail.com>
2021-02-12 16:33:13 -0700

    Implement computeIDF()

--- src/main/java/com/bruce/jobmatchr/parse/TFIDF.java
+++ src/main/java/com/bruce/jobmatchr/parse/TFIDF.java
@@ -3,6 +3,7 @@ package com.bruce.jobmatchr.parse;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.List;
 import java.util.Set;
@@ -53,8 +54,47 @@ public class TFIDF {
         return tfHash;
     }
 
-    public Hashtable<String, Double> computeIDF() {
+    public Hashtable<String, Double> computeIDF(List<String> wordsFromResume, List<String> wordsFromJobDescription) {
+        Hashtable<String, Double> idfHash = new Hashtable<String, Double>();
 
+        // Create a list of hash tables.
+        List<Hashtable<String, Double>> listOfHashes = new ArrayList<Hashtable<String, Double>>();
+
+        // Add the frequency tables of words of each document to the list.
+        listOfHashes.add(getFrequencyByWord(wordsFromResume));
+        listOfHashes.add(getFrequencyByWord(wordsFromJobDescription));
+
+        int numOfDocuments = 2;
+
+        // Set all values in the idf hash table to be 0.
+        for (String word : listOfHashes.get(0).keySet()) {
+            idfHash.put(word, 0.0);
+        }
+
+        // Count the number of documents with a term t (word) in it.
+        for (Hashtable<String, Double> hashtable : listOfHashes) {
+            for (String word : hashtable.keySet()) {
+                if (hashtable.get(word) > 0) {
+                    idfHash.put(word, (double) idfHash.get(word) + 1);
+                }
+            }
+        }
+
+        // IDF(t) = ln(Total number of documents / Number of documents with term t in
+        // it).
+        for (String word : idfHash.keySet()) {
+            double numDocWithTermT = idfHash.get(word);
+            double idf = Math.log((double) numOfDocuments / numDocWithTermT);
+
+            // Replace divide by 0 errors with a 0.
+            if (numDocWithTermT == 0)
+                idfHash.put(word, 0.0);
+            else
+                idfHash.put(word, idf);
+
+        }
+
+        return idfHash;
     }
 
     public Hashtable<String, Double> computeTFIDF(Hashtable<String, Double> tf) {
